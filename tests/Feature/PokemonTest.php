11f67144bd6de7803220a68c664a56b4eab4a79a
John Ber <32464352+JhnBer@users.noreply.github.com>
2024-02-12 18:54:02 +0400

    Create PokemonTest.php

---
+++ tests/Feature/PokemonTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\PokemonShapes;
+use App\Models\Ability;
+use App\Models\Location;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Tests\TestCase;
+
+class PokemonTest extends TestCase
+{
+    use WithFaker;
+    // use RefreshDatabase;
+
+    public function test_create_pokemon(): void
+    {
+        $abilities = [];
+        for ($i=0; $i < rand(1, 2); $i++) { 
+            $abilities[] = Ability::whereNot('id', $abilities)
+                                ->inRandomOrder()
+                                ->first()
+                                ->id;
+        }
+
+        $data = [
+            'name' => $this->faker->name(),
+            'shape' => $this->faker->randomElement(PokemonShapes::array()),
+            'location_id' => Location::inRandomOrder()->first()->id,
+            'abilities' => $abilities,
+            'image' => UploadedFile::fake()->image('pokemon.jpeg'),
+        ];
+
+        $response = $this->postJson(route('pokemon.store'), $data);
+        $response->assertCreated();
+    }
+}
